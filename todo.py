6e42cefab5508eed4135855aa32a8d2679e7137c
harshitha414 <harshithaprakash411@gmail.com>
2025-05-28 19:36:05 +0530

    Create todo.py

---
+++ todo.py
@@ -0,0 +1,13 @@
+todo = ['Get quarters',
+        'Do Laundry.',
+        'Take a Walk.',
+        'Get a haircut.',
+        'Make some tea.',
+        'Complete lists chapter.',
+        'Call mom.',
+        'Watch my hero academia.']
+#use slicing to print first two elements in the list:
+
+print(todo[0:2])
+
+
