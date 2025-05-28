44d558f6d75555fbb3c33fa9a359e8dfabf87d3d
harshitha414 <harshithaprakash411@gmail.com>
2025-05-28 20:44:11 +0530

    Create fun.py

---
+++ fun.py
@@ -0,0 +1,22 @@
+#This is a function to print the details given by theuser.
+print('Welcome to Codedex, Where coding is made Fun and informative')
+
+#This is a function to input the string values:
+name = (input("Enter Your name:"))
+print(name)
+
+#This is a function used to input integer values from user:
+num = int(input('Enter a Digit of your choice:'))
+print(num)
+
+#This is a function to print out the maximum variable in the list:
+a = [ 2, 4 , 10 , 108 , 16700]
+print(max(a))
+
+#This  is a funnction used to print the length of the given list:
+vowels = ['a' , 'e' , 'i' , 'o' , 'u']
+print(len(vowels))
+
+#A function which we havent seen before:
+b = [1,5,10]
+print(sum(b))
