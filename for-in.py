65b7b2acb85f9a5fc6c4261c6e968d2c09ef1a92
harshitha414 <harshithaprakash411@gmail.com>
2025-05-28 20:14:12 +0530

    Create for-in.py

---
+++ for-in.py
@@ -0,0 +1,9 @@
+playlist = ['Porches - rangerover',
+            'Mount Eerie - You Swan, Go On',
+            'CarolynPolachek - Look at Me Now',
+            'Pinegrove - Darkenss',
+            'LVL UP - Spirit Was',
+            'Mitski- First Love / Late Spring']
+
+for i in range(len(playlist)):
+  print(playlist[i])
