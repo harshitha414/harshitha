0c45abc058e890e5b9ca7d3acaf80c2dc76489f4
harshitha414 <harshithaprakash411@gmail.com>
2025-05-30 21:57:17 +0530

    Create calci.py

---
+++ calci.py
@@ -0,0 +1,21 @@
+def add(a,b):
+  value= a+b
+  return value
+def subract(a,b):
+  value = a-b
+  return value
+def multiply(a,b):
+  value =  a*b
+  return value
+def divide(a,b):
+  value = a/b
+  return value
+def exp(a,b):
+  value = a**b
+  return value
+
+print(add(4,5))
+print(subract(4,5))
+print(multiply(4,5))
+print(divide(4,5))
+print(exp(4,5))
