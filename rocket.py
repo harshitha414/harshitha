35f573750f19b72e4f87862083c1ef89d9fb0a9e
harshitha414 <harshithaprakash411@gmail.com>
2025-05-30 21:56:53 +0530

    Create rocket.py

---
+++ rocket.py
@@ -0,0 +1,8 @@
+def distance_to_miles(distance_in_kilometers):
+  value = distance_in_kilometers/1.609
+  print(value)
+
+#To call the function and use 10000 as an arguement:
+
+arguement_answer = distance_to_miles(10000)
+
